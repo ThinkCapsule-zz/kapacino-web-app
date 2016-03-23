78c0dfad5d06276c96193c04f5a634ff01cbc34a
Michael <m.ding5692@gmail.com>
2016-03-22 23:11:14 -0400

    Finished off article

--- Desktop/angularfire-seed-master/app/articles/articles.js
+++ Desktop/angularfire-seed-master/app/articles/articles.js
@@ -1,13 +1,36 @@
 "use strict";
-angular.module('myApp.articles', ['firebase.utils', 'firebase.auth', 'ngRoute'])
-
+angular.module('myApp.articles', ['firebase.utils', 'firebase.auth', 'ngRoute','contentful'])
+  //https://docs.angularjs.org/tutorial/step_07
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/articles', {
-      controller: 'ArticlesCtrl',
-      templateUrl: 'articles/articles.html'
-    });
+	    $routeProvider.
+      when('/articles', {
+	      controller: 'ArticlesCtrl',
+	      templateUrl: 'articles/articles.html'
+	    }).
+      when('/article', {
+        controller: 'ArticleCtrl',
+        templateUrl: 'articles/article.html'
+      });
+ 	}])
+  .config(function(contentfulProvider) {
+	    contentfulProvider.setOptions({
+	        space: '1oq1lgnwupsh',
+	        accessToken: '9e202ccaa19212267e8a417356d41277c51ea827b2a1122c00f8f3ea0c25dfe3'
+	    });
+ 	})
+  .controller('ArticlesCtrl', ['$scope', 'Auth', '$location', 'fbutil', function($scope, Auth, $location, fbutil, contentful) {
+
+
   }])
-
-  .controller('ArticlesCtrl', ['$scope', 'Auth', '$location', 'fbutil', function($scope, Auth, $location, fbutil) {
-
+  .controller('ArticleCtrl', ['$scope', 'commentList', function($scope, commentList) {
+      $scope.comments = commentList;
+      $scope.addComment = function(newComment) {
+        if( newComment ) {
+          $scope.comments.$add({text: newComment});
+        }
+      };
+    }])
+  .factory('commentList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
+    var ref = fbutil.ref('comments').limitToLast(10);
+    return $firebaseArray(ref);
   }]);
